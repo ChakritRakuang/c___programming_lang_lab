8a341027baffa1d466f935b18e84f571f7694262
Chakrit  Rakhuang <chakrit.rakhuang@gmail.com>
2017-07-25 16:42:05 +0700

    เพิ่มคำอธิบาย twp.cpp ในโปรแกรม

---
+++ two.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Chakrit  Rakhuang on 7/25/2017.
+//
+
+// Using namespace std
+
+#include <iostream>
+
+int main()
+{
+    std :: cout << "Hello c++ \n";
+    std :: cout << "My name is Chakrit\n";
+
+    return 0;
+}
