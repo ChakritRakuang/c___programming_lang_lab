2f4557e4a59a68a63cca96eb8a4ad564d68d576a
Chakrit  Rakhuang <chakrit.rakhuang@gmail.com>
2017-07-25 17:03:31 +0700

    delect variables.cpp add four.cpp

---
+++ four.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Chakrit  Rakhuang on 7/25/2017.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "This is a girl." << endl;
+
+    bool is_playing = true;
+    cout << "Playing = " << is_playing << endl;
+    cout << "Playing invert = " << !is_playing << endl;
+
+    cout << "1 and True = " << ((1 != 0)) << endl;
+    cout << "True and True = " << true << endl;
+    cout << "True and False = " << false << endl;
+    cout << "True or True = " << true << endl;
+    cout << "True or False = " << true << endl;
+
+    return 0;
+}
--- variables.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Chakrit  Rakhuang on 7/25/2017.
-//
-
-int a = 3;
-int b = 5;
-
-int sum = a+b;
